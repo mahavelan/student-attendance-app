e477e291ff7ba5518da6a465f10c04a771048796
mahavelan <mahamahalakshmi1112@gmail.com>
2025-07-11 23:22:22 +0530

    Create App.js

---
+++ frontend/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import CameraCapture from "./CameraCapture";
+
+function App() {
+  return (
+    <div>
+      <h1>📸 Face Attendance App</h1>
+      <CameraCapture />
+    </div>
+  );
+}
+
+export default App;

bc6bb2b805817ce23059bfa9aa9cbee38d7dea11
mahavelan <mahamahalakshmi1112@gmail.com>
2025-07-11 18:01:05 +0530

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,1 +1,14 @@
+import React from "react";
+import CameraCapture from "./CameraCapture";
+
+function App() {
+  return (
+    <div style={{ textAlign: "center", marginTop: "50px" }}>
+      <h1>📸 Student Attendance Camera App</h1>
+      <CameraCapture />
+    </div>
+  );
+}
+
+export default App;
 
